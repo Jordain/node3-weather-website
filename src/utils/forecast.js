e50479bbbefc54d88062b7b9efbe0ff527486aa3
Jordain Perreault-Joyce <jperreault-joyce@absoluteresults.com>
2019-04-17 16:46:44 -0700

    updated forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,14 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/c098081b8df26ea458af6130b27a8528/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si'
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body.daily.data)
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + 
+            body.currently.precipProbability + '% chance of rain. High temperature of ' + body.daily.data[0].temperatureHigh + '. Low temperature of ' + body.daily.data[0].temperatureLow)
         }
     })
 }
